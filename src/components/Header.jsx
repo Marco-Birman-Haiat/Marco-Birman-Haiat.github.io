2e712fd08c04c7558b6e27ef63559c8f9fd4b368
Marco Haiat <marco.haiat@gmail.com>
2023-04-03 09:59:24 -0300

    feat: adiciona links a no header, gitHub e linkedIn

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,8 +10,12 @@ function Header() {
             <ImHome3 className='text-slate-300 text-3xl hover:scale-110 hover:brightness-110'/>
           </Link>
           <div className='flex gap-6'>
-            <ImGithub className='text-slate-300 text-3xl hover:scale-110 hover:brightness-110'/>
-            <ImLinkedin className='text-slate-300 text-3xl hover:scale-110 hover:brightness-110'/>
+            <a href='https://github.com/Marco-Birman-Haiat' target='_blank'>
+              <ImGithub className='text-slate-300 text-3xl hover:scale-110 hover:brightness-110'/>
+            </a>
+            <a href='https://www.linkedin.com/in/marco-birman-haiat/' target='_blank'>
+              <ImLinkedin className='text-slate-300 text-3xl hover:scale-110 hover:brightness-110'/>
+            </a>
           </div>
       </header>
     </>
